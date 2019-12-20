ee26ffe6dfde65f32613a3e7c1d30ec3acf1fe30
Lilit <lilit.mazmanyan1@gmail.com>
2019-12-20 22:13:22 +0400

    Added 3rd comment

--- src/fgh.java
+++ src/fgh.java
@@ -5,7 +5,9 @@ public class fgh {
         System.out.println(isPalindrome.isPalindrome(12321));
         System.out.println(ReverseInteger.reverse(123));
         System.out.println(ValodParentheses.isValid("{[)}"));
-  // Comment1 for master
+  // Comment 1 for master
   // Comment 2 for master
+  // Comment 3 for master
+
     }
 }
